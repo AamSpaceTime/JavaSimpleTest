588bfefcd781f925e62d0fbf44ef567ccae091f3
Андрей <@Pobeda>
2018-07-03 15:08:00 +0300

    Тестовая фиксация

--- src/javasimpletest/JavaSimpleTest.java
+++ src/javasimpletest/JavaSimpleTest.java
@@ -7,6 +7,7 @@ package javasimpletest;
 
 import java.util.Scanner;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
 import java.io.OutputStream;
@@ -99,7 +100,7 @@ public class JavaSimpleTest {
 
 		conn.disconnect();
             
-    } catch(Exception e) {
+    } catch(IOException | RuntimeException e) {
         System.out.println(e.getMessage());
     }
         
